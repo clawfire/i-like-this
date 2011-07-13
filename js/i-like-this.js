590d043582e57e4cfb2add66581c50e767a0e460
thibault milan <clawfire@gmail.com>
2011-07-13 15:00:48 +0200

    typo and callback text manipulation / class improuvment

--- js/i-like-this.js
+++ js/i-like-this.js
@@ -4,7 +4,8 @@ function likeThis(postId) {
 		
 		jQuery.post(blogUrl + "/wp-content/plugins/i-like-this/like.php",{ id: postId },function(data){
 				jQuery('#'+postId+'.ilt_counter_widget_counter').text(data);
-				jQuert('.ilt_counter_widget_btn').text('Voted !')
+				jQuery('.ilt_counter_widget_btn').text('Voted').addClass('liked');
+				
 			});
 	}
 }
